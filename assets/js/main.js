f00a3560ea2608f94832d857b47b9af97720e47e
giuseppesciacca <sciaccagi@gmail.com>
2023-03-17 17:58:33 +0100

    [fix] header

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,7 +9,7 @@ con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 
 con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
 con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe; */
 
-const rowEl = document.querySelector('.row');
+const rowEl = document.getElementById('row_main');
 const btn_play = document.getElementById('btn_play');
 const difficultyEl = document.getElementById('difficulty');
 
