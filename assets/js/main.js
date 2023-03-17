9149194e46cf0b98cf2e900ef80d43f056b0bf7a
giuseppesciacca <sciaccagi@gmail.com>
2023-03-17 17:30:13 +0100

    [add] difficulty select (bonus)

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,17 +3,43 @@ Ogni cella ha un numero progressivo, da 1 a 100.
 Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata. */
 
+/* Bonus
+Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
+con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
+con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
+con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe; */
+
 const rowEl = document.querySelector('.row');
 const btn_play = document.getElementById('btn_play');
+const difficultyEl = document.getElementById('difficulty');
 
 btn_play.addEventListener('click', function () {
-    let numSquare = 100; //mettere numero attraverso select
+    rowEl.innerHTML = '';   //svuoto il precedente campo
+
+    let numSquare = 0; //metto numero attraverso select
+    if (difficultyEl.value == 'easy') {
+        numSquare = 100;
+    } else if (difficultyEl.value == 'medium') {
+        numSquare = 81;
+    } else {
+        numSquare = 49;
+    }
 
     for (let i = 0; i < numSquare; i++) {
         const squareEl = document.createElement('div');
         squareEl.classList.add('col_my', 'd-flex', 'align-items-center', 'justify-content-center', 'fs-5');
 
+        if (difficultyEl.value == 'easy') {
+            squareEl.classList.add('col_my_ez')
+        } else if (difficultyEl.value == 'medium') {
+            squareEl.classList.add('col_my_md')
+        } else {
+            squareEl.classList.add('col_my_hd')
+        }
+
         rowEl.append(squareEl);
+
+
         squareEl.innerHTML = `<span> ${i + 1} </span>`;
 
         //al click, coloro + log;
@@ -22,4 +48,5 @@ btn_play.addEventListener('click', function () {
             console.log(`Hai cliccato la cella numero ${i + 1}`);
         });
     }
-})
+
+});
